d939ca02230f491d55ae2d720626f8f92cedfd39
n0k1b <nokibevon7@gmail.com>
2020-04-01 00:17:48 +0600

    inital commit by nokib

---
+++ database/migrations/2020_03_30_134806_create_questions_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateQuestionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('questions', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('topic_id');
+            $table->string('question',400);
+            $table->integer('board_id')->nullable();
+            $table->string('option1');
+            $table->string('option2');
+            $table->string('option3');
+            $table->string('option4');
+            $table->string('correct_option');
+            $table->string('tag')->nullable();
+            $table->string('details',500)->nullable();
+            $table->timestamps();
+            $table->foreign('topic_id')
+            ->references('id')->on('topics')
+            ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('questions');
+    }
+}
